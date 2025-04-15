3fa93bc23b5b8497763727ade61744696a50407a
Md. Arif <arifmia1129@gmail.com>
2025-04-15 17:21:38 +0600

    Input format Only one line containing a number N .  Output Format Print the answer according to the required above  Sample Input 1 5  Sample Output 1 5 4 3 2 1  4 3 2 1  3 2 1  2 1  1

--- extra_pattern_related_problems.c
+++ extra_pattern_related_problems.c
@@ -1,3 +1,25 @@
+// You're given N as input. Print the following pattern given in testcases.
+
+
+
+// #include <stdio.h>
+
+// int main () {
+//     int num;
+
+//     scanf("%d", &num);
+
+//     for(int i = 1; i <= num; i++) {
+//         for(int j = 1; j <= i; j++) {
+//             printf("%d ", j);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
 int main () {
@@ -5,9 +27,13 @@ int main () {
 
     scanf("%d", &num);
 
-    for(int i = 1; i <= num; i++) {
-        for(int j = 1; j <= i; j++) {
+    for(int i = num; i >= 1; i--) {
+
+        int j = i;
+
+        while(j > 0) {
             printf("%d ", j);
+            j--;
         }
         printf("\n");
     }
