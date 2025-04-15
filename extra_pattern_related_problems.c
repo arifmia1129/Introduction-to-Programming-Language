194dfe867d1663d7b4b709f000305487ca3d2965
Md. Arif <arifmia1129@gmail.com>
2025-04-15 19:07:46 +0600

    another pattern problem solved

--- extra_pattern_related_problems.c
+++ extra_pattern_related_problems.c
@@ -20,6 +20,27 @@
 // }
 
 
+// #include <stdio.h>
+
+// int main () {
+//     int num;
+
+//     scanf("%d", &num);
+
+//     for(int i = num; i >= 1; i--) {
+
+//         int j = i;
+
+//         while(j > 0) {
+//             printf("%d ", j);
+//             j--;
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main () {
@@ -27,13 +48,9 @@ int main () {
 
     scanf("%d", &num);
 
-    for(int i = num; i >= 1; i--) {
-
-        int j = i;
-
-        while(j > 0) {
-            printf("%d ", j);
-            j--;
+    for(int i = 1; i <= num; i++) {
+        for(int j = 1; j <= i; j++) {
+            printf("%c ", 65 + i - 1);
         }
         printf("\n");
     }
