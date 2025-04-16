0fbcab2989196300d5d901aa88a03b85d8570f6b
Md. Arif <arifmia1129@gmail.com>
2025-04-16 19:09:36 +0600

    Array element access by index

---
+++ array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int arr[3];
+
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+
+    printf("%d, %d, %d", arr[0], arr[1], arr[2]);
+
+    return 0;
+}
