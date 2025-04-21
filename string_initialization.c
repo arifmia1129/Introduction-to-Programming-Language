fa97b019fcfc8cf356fb14fc2fc79c53e356a236
Md. Arif <arifmia1129@gmail.com>
2025-04-21 10:04:44 +0600

    String initialization

---
+++ string_initialization.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    // char s[5] = {'A', 'R', 'I', 'F'};
+
+    // char s[5] = "ARIF";
+
+    char full_name[30] = "Md. Arif Mia";
+
+    printf("%s", full_name);
+
+    return 0;
+}
