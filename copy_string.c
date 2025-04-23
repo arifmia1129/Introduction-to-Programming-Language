34e76b6a26227df5bff922ecaa8d90221aadc0bb
Md. Arif <arifmia1129@gmail.com>
2025-04-23 09:17:00 +0600

    String copy

---
+++ copy_string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+    char s1[101], s2[101];
+
+    scanf("%s %s", &s1, &s2);
+
+    int s2_lng = strlen(s2);
+
+    for(int i = 0; i <= s2_lng; i++) {
+        s1[i] = s2[i]; 
+    }
+
+    printf("%s %s", s1, s2);
+
+    return 0;
+}
