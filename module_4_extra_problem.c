179923eed559603ab3533d8c813ddac340c3aa59
Md. Arif <arifmia1129@gmail.com>
2025-05-05 19:24:46 +0600

    4 types of function problem practice done

---
+++ module_4_extra_problem.c
@@ -0,0 +1,95 @@
+// #include <stdio.h>
+
+// int isEven (int num) {
+//     if(num % 2 == 0) {
+//         return 1;
+//     }else {
+//         return 0;
+//     }
+// }
+
+// int main () {
+
+//     int res = isEven(3);
+
+//     if(res) {
+//         printf("Even");
+//     }else{
+//         printf("Odd");
+//     }
+    
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int isEven () {
+//     int num;
+
+//     scanf("%d", &num);
+
+//     if(num % 2 == 0) {
+//         return 1;
+//     }else {
+//         return 0;
+//     }
+// }
+
+// int main () {
+
+//     int res = isEven();
+
+//     if(res) {
+//         printf("Even");
+//     }else{
+//         printf("Odd");
+//     }
+    
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// void isEven () {
+//     int num;
+
+//     scanf("%d", &num);
+
+//     if(num % 2 == 0) {
+//         printf("Even");
+//     }else {
+//        printf("Odd");
+//     }
+// }
+
+// int main () {
+
+//      isEven();
+    
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// void isEven (int num) {
+    
+
+//     if(num % 2 == 0) {
+//         printf("Even");
+//     }else {
+//        printf("Odd");
+//     }
+// }
+
+// int main () {
+//     int num;
+
+//     scanf("%d", &num);
+
+//      isEven(num);
+    
+//     return 0;
+// }
