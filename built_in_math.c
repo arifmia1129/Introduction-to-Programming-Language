28fa6c3e36db27d066358956c371eef3203ca410
Md. Arif <arifmia1129@gmail.com>
2025-05-05 18:31:11 +0600

    Built in math functions

---
+++ built_in_math.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main () {
+    double num = 4.5;
+
+    // int ans = ceil(num);
+    // int ans = floor(num);
+    // int ans = round(num);
+    // int ans = sqrt(num);
+    // int ans = pow(3, 2);
+    int ans = abs(-3);
+
+    printf("%d\n", ans);
+    
+    return 0;
+}
